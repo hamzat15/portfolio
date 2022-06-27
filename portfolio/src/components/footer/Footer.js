50acca59067283fd6cfa0a228a9121c46b27f3c3
hamzat <hamzat1.siby@epitech.eu>
2022-06-27 12:27:41 +0200

    add background and footer style

--- portfolio/src/components/footer/Footer.js
+++ portfolio/src/components/footer/Footer.js
@@ -2,10 +2,15 @@ import React from 'react';
 import "./style.css"
 
 const Footer = () => {
+    var imageName = require('./herb.png')
+
     return (
+        <>
+            <div className='herbe'><img src={imageName} /></div>
         <div className='footer'>
             <p>footer</p>
         </div>
+        </>
     );
 };
 
